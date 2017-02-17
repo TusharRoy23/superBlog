f0c3f381e98047198a67db473ecaf58496f3e1f6
tusharroy23 <risenboy23@gmail.com>
2017-02-18 03:09:39 +0600

    dashboard update 1

---
+++ dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Blog</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.css">
+		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap-theme.css">
+		<script type="text/javascript" src="/superBlog/assets/js/jquery.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.8.0.min.js"></script>
+		<link rel="stylesheet" href="/superBlog/style/style.css">
+	</head>
+	<body>
+		<div class="col-lg-12 col-sm-12 col-md-12 leftRightPaddingSetZero" id="dashboard">
+			<?php 
+				include_once('header.php');
+			?>
+			<div class="col-lg-12 col-md-12 col-sm-12" style="padding-top: 8%;">
+				<div class="col-lg-3 col-md-3 text-center hidden-xs hidden-sm" style="background-color: green;">
+					menu
+				</div>
+				<div class="col-lg-8 col-md-8 col-sm-12 leftRightPaddingSetZero" style="">
+					<div class="col-lg-12 col-sm-12 col-md-12 text-center stopWordBreaking" style="font-size: 150%;">
+						jsdafffffffffffffffffffffffffffffffffffffffffffffffffffdfasdffffffffffffffffaefaeeeeeeeeeeeeeeeeeeeeeeeeefff
+					</div>
+					<div class="col-lg-12 col-sm-12 col-md-12 stopWordBreaking text-center" style="font-size: 110%; margin-top: 3%;">
+						faldfadf,sadffffffffpdfasdfkadfk[adf[adlfa[dslf[asdlfsdlfsfksflkfdldkfgjdfghgksfjgksdfmgpsgldfsmgsdlfmgsfdmgsdfmgjgsmgsmdfggmpsmgpsmgp]]]]
+						faldfadf,sadffffffffpdfasdfkadfk[adf[adlfa[dslf[asdlfsdlfsfksflkfdldkfgjdfghgksfjgksdfmgpsgldfsmgsdlfmgsfdmgsdfmgjgsmgsmdfggmpsmgpsmgp]]]]
+						<br />
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 stopWordBreaking text-center" style="background-color: blue;">
+						comments
+					</div>
+				</div>
+			</div>	
+		</div>
+		<script type="text/javascript" src="/superBlog/assets/customJs/sideBar.js"></script>
+	</body>
+</html>
