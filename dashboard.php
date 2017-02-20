37438c53399ab49b4ea395a6791a654ec4bb9527
tusharroy23 <risenboy23@gmail.com>
2017-02-20 18:11:35 +0600

    dashboard update 2

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,12 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+	include_once("DBconfig.php");
+	if(empty($_SESSION)){
+		$_SESSION['errMsg'] = "<font color ='red'><b>You have to Login First</b>";
+		header('Location: dashboard.php');
+	}
+?>
 <html>
 	<head>
 		<title>Blog</title>
@@ -9,33 +17,31 @@
 		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap-theme.css">
 		<script type="text/javascript" src="/superBlog/assets/js/jquery.js"></script>
-		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.8.0.min.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.9.1.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="/superBlog/style/style.css">
 	</head>
 	<body>
 		<div class="col-lg-12 col-sm-12 col-md-12 leftRightPaddingSetZero" id="dashboard">
 			<?php 
+			//echo $_SESSION['userID'];
+			if($_SESSION['userID'] != null){
+
+				include_once('headerAfterLogin.php');
+			}
+			else{
 				include_once('header.php');
+				$_SESSION['userID'] = null;
+			}
 			?>
-			<div class="col-lg-12 col-md-12 col-sm-12" style="padding-top: 8%;">
-				<div class="col-lg-3 col-md-3 text-center hidden-xs hidden-sm" style="background-color: green;">
-					menu
-				</div>
-				<div class="col-lg-8 col-md-8 col-sm-12 leftRightPaddingSetZero" style="">
-					<div class="col-lg-12 col-sm-12 col-md-12 text-center stopWordBreaking" style="font-size: 150%;">
-						jsdafffffffffffffffffffffffffffffffffffffffffffffffffffdfasdffffffffffffffffaefaeeeeeeeeeeeeeeeeeeeeeeeeefff
-					</div>
-					<div class="col-lg-12 col-sm-12 col-md-12 stopWordBreaking text-center" style="font-size: 110%; margin-top: 3%;">
-						faldfadf,sadffffffffpdfasdfkadfk[adf[adlfa[dslf[asdlfsdlfsfksflkfdldkfgjdfghgksfjgksdfmgpsgldfsmgsdlfmgsfdmgsdfmgjgsmgsmdfggmpsmgpsmgp]]]]
-						faldfadf,sadffffffffpdfasdfkadfk[adf[adlfa[dslf[asdlfsdlfsfksflkfdldkfgjdfghgksfjgksdfmgpsgldfsmgsdlfmgsfdmgsdfmgjgsmgsmdfggmpsmgpsmgp]]]]
-						<br />
-					</div>
-					<div class="col-lg-12 col-md-12 col-sm-12 stopWordBreaking text-center" style="background-color: blue;">
-						comments
-					</div>
-				</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero" style="margin-top: 8%;">
+				<?php
+					include_once('allThreads.php');
+				?>
 			</div>	
 		</div>
 		<script type="text/javascript" src="/superBlog/assets/customJs/sideBar.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/customJs/readView.js"></script>
 	</body>
 </html>
