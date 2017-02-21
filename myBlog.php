cd89d670c171ae26fc8681c512a9f17a7d9bc9a8
tusharroy23 <risenboy23@gmail.com>
2017-02-21 20:44:29 +0600

    myBlog update 2

--- myBlog.php
+++ myBlog.php
@@ -13,34 +13,36 @@
 	$valid = true;
 	//$submitBtnClick = $_POST["upload"];
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$valid = true;
-		if(empty($_POST["title"]))
-		{
-			$valid = false;
-			$titleErr = "title required";
-		}
-		else{
-			$title = check_input($_POST["title"]);
-		}
-		if(empty($_POST["description"])){
-			$valid = false;
-			$descriptionErr = "description required";
-		}
-		else{
-			$description = check_input($_POST["description"]);
-		}
-		if($valid == true){
-			$userID = $_SESSION['userID'];
-			$date = date("Y/m/d");
-			date_default_timezone_set("Asia/Dhaka");
-			$time = date("h:i a");
+		if (isset($_POST['upload'])) {
+			$valid = true;
+			if(empty($_POST["title"]))
+			{
+				$valid = false;
+				$titleErr = "title required";
+			}
+			else{
+				$title = check_input($_POST["title"]);
+			}
+			if(empty($_POST["description"])){
+				$valid = false;
+				$descriptionErr = "description required";
+			}
+			else{
+				$description = check_input($_POST["description"]);
+			}
+			if($valid == true){
+				$userID = $_SESSION['userID'];
+				$date = date("Y/m/d");
+				date_default_timezone_set("Asia/Dhaka");
+				$time = date("h:i a");
 
-			$sql = "INSERT INTO threads (userID, uploadDate, uploadTime, threadTitle, threadContent, updateDate, updateTime) values (?, ?, ?, ?, ?, ?, ?)";
-			$stmt = $conn->prepare($sql);
-			$stmt->bind_param("issssss", $userID, $date, $time, $title, $description, $date, $time);
-			if($stmt->execute()){
-				$_SESSION['uploadSuccessful'] = "<font color='green'><b>Successfully Uploaded !!</b>";
-				$stmt->close();
+				$sql = "INSERT INTO threads (userID, uploadDate, uploadTime, threadTitle, threadContent, updateDate, updateTime) values (?, ?, ?, ?, ?, ?, ?)";
+				$stmt = $conn->prepare($sql);
+				$stmt->bind_param("issssss", $userID, $date, $time, $title, $description, $date, $time);
+				if($stmt->execute()){
+					$_SESSION['uploadSuccessful'] = "<font color='green'><b>Successfully Uploaded !!</b>";
+					$stmt->close();
+				}
 			}
 		}
 	}
@@ -56,43 +58,50 @@
 		<title>My Blog</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.css">
-		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/superBlog/assets/css/bootstrap-theme.css">
 		<script type="text/javascript" src="/superBlog/assets/js/jquery.js"></script>
-		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.8.0.min.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.9.1.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="/superBlog/style/style.css">
+		<script type="text/javascript" src="/superBlog/assets/customJs/sideBar.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/customJs/readView.js"></script>
 	</head>
 	<body>
 		<div class="col-lg-12 col-sm-12 col-md-12 leftRightPaddingSetZero" id="dashboard">
 			<?php 
 				include_once('headerAfterLogin.php');
 			?>
-			<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero" style="margin-top: 8%;">
-				<div class="container" style="">
-					<span>
-            			<?php if(!empty($_SESSION['uploadSuccessful'])) { echo $_SESSION['uploadSuccessful']; } ?>		
-       					<?php //unset($_SESSION['uploadSuccessful']); ?>
-					</span>
-					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-						<h4>Title</h4>
-						<input type="text" name="title" class="form-control">
-						<span class="error"><?php echo $titleErr;?></span><br />
-						<h4>Description</h4>
-						<textarea class="form-control" rows="7" name="description"></textarea>
-						<span class="error"><?php echo $descriptionErr;?></span><br /><br />
-						<input type="submit" name="upload" value="upload" class="btn btn-success btn-block">
-					</form>
+			<div class="col-sm-12 col-lg-12 col-md-12" style="margin-top: 8%;">
+				<div class="col-lg-4 col-md-4 col-sm-4" style="">
+					<?php include_once('profile.php'); ?>
 				</div>
-			</div>
-			<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero" style="margin-top: 5%; ">
-				<div class="container leftRightPaddingSetZero" style="">
-					<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero">
-						<?php
-							include_once("threads.php");
-							//$_SESSION['userID'] = $_SESSION['userID'];
-						?>
+				<div class="col-lg-8 col-md-8 col-sm-8">
+					<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero" style="">
+						<div class="" style="">
+							<span>
+		            			<?php if(!empty($_SESSION['uploadSuccessful'])) { echo $_SESSION['uploadSuccessful']; } ?>		
+		       					<?php //unset($_SESSION['uploadSuccessful']); ?>
+							</span>
+							<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+								<h4>Title</h4>
+								<input type="text" name="title" class="form-control">
+								<span class="error"><?php echo $titleErr;?></span><br />
+								<h4>Description</h4>
+								<textarea class="form-control" rows="7" name="description"></textarea>
+								<span class="error"><?php echo $descriptionErr;?></span><br /><br />
+								<input type="submit" name="upload" value="upload" class="btn btn-success btn-block">
+							</form>
+						</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero" style="margin-top: 5%; ">
+						<div class="leftRightPaddingSetZero" style="">
+							<div class="col-lg-12 col-md-12 col-sm-12 leftRightPaddingSetZero">
+								<?php
+									include_once("threads.php");
+									//$_SESSION['userID'] = $_SESSION['userID'];
+								?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
