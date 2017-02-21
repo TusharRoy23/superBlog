cf6e75519e3297aef7640b67574e566b6ce16c57
tusharroy23 <risenboy23@gmail.com>
2017-02-21 20:51:38 +0600

    side bar update 1

--- assets/customJs/sideBar.js
+++ assets/customJs/sideBar.js
@@ -1,9 +1,9 @@
-function onOpen(){
+function onopens(){
 	document.getElementById("sideBar").style.width = "38%";
     document.getElementById("dashboard").style.marginLeft = "32%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
-function onClose(){
+function oncloses(){
 	document.getElementById("sideBar").style.width = "0";
     document.getElementById("dashboard").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
