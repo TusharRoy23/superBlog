c0f066893e09f2045442b970140ec83b5d130dc1
tusharroy23 <risenboy23@gmail.com>
2017-02-21 20:47:43 +0600

    threadEdit update 1

---
+++ threadEdit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Edit</title>
+		<!-- <meta charset="utf-8"> -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="text/javascript" src="/superBlog/assets/js/jquery.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/jquery-1.9.1.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="/superBlog/assets/style/style.css">
+		<!-- <script type="text/javascript" src="/superBlog/assets/customJs/sideBar.js"></script>
+		<script type="text/javascript" src="/superBlog/assets/customJs/readView.js"></script> -->
+	</head>
+	<body>
+		<div class="col-lg-12 col-sm-12 col-md-12 leftRightPaddingSetZero" id="dashboard">
+			<?php include_once('headerAfterLogin.php'); ?>
+		</div>
+	</body>
+</html>
